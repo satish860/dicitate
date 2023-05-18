1b0085cad6aa5fd8b9a062411000831e3a59f252
apprakash <101494351+apprakash@users.noreply.github.com>
2023-05-18 12:17:39 +0530

    Title change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-            <Component {...pageProps} />
-    </ChakraProvider>
-    
+    <>
+      <Head>
+        <title>SpeakWise</title>
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
